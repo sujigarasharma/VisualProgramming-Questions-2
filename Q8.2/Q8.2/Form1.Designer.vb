12d07a231ec3140627adb2446f083a7bcf0ff413
Sujigarasharma K <114681230+sujigarasharma@users.noreply.github.com>
2023-03-09 23:30:34 +0530

    Add files via upload

---
+++ Q8.2/Q8.2/Form1.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Font = New System.Drawing.Font("Calibri", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TreeView1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.TreeView1.Location = New System.Drawing.Point(17, 78)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(333, 341)
+        Me.TreeView1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Cambria", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Maroon
+        Me.Label1.Location = New System.Drawing.Point(12, 34)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(381, 28)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Winter 2021 Visual Programming"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.documents_documents_accounting_assessment_assignment_background_bureaucracy_busy_118208416
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(643, 431)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ Q8.2/Q8.2/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Dim tNode As TreeNode
+        tNode = TreeView1.Nodes.Add("Desktop")
+
+        TreeView1.Nodes(0).Nodes.Add(" Winter 2021 Visual Programming")
+        TreeView1.Nodes(0).Nodes.Add("Assignments")
+        TreeView1.Nodes(0).Nodes(1).Nodes.Add("DA1")
+        TreeView1.Nodes(0).Nodes(1).Nodes.Add("DA2")
+        TreeView1.Nodes(0).Nodes(1).Nodes.Add("DA3")
+        TreeView1.Nodes(0).Nodes.Add("Test")
+        TreeView1.Nodes(0).Nodes(2).Nodes.Add("Test 1")
+        TreeView1.Nodes(0).Nodes(2).Nodes.Add("Test 2")
+
+        TreeView1.Nodes(0).Nodes.Add("Quiz")
+        TreeView1.Nodes(0).Nodes(3).Nodes.Add("Quiz1")
+        TreeView1.Nodes(0).Nodes(3).Nodes.Add("Quiz2")
+        TreeView1.Nodes(0).Nodes(3).Nodes.Add("Quiz3")
+        TreeView1.Nodes(0).Nodes.Add("Problem Solving")
+        TreeView1.Nodes(0).Nodes(4).Nodes.Add("PS1")
+        TreeView1.Nodes(0).Nodes(4).Nodes.Add("PS2")
+
+    End Sub
+End Class
